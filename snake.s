61ea2ebe82f4183e38b2262d93cb94dd8520dcf2
changknn <changknn@foxmail.com>
2022-04-29 19:41:34 +0800

    final version

--- snake.s
+++ snake.s
@@ -2,14 +2,13 @@
         .equ SWI_CheckBlcak, 0x202    @check black button
         .equ  SWI_CheckBlue, 0x203    @check press Blue button
         .equ  SWI_DRAW_STRING, 0x204    @display a string on LCD
+        .equ SWI_DRAW_INT, 0x205     @display an int on LCD
         .equ  SWI_CLEAR_DISPLAY, 0x206    @clear LCD
         .equ  SWI_DRAW_CHAR, 0x207    @display a char on LCD
         .equ SWI_Exit, 0x11
         .equ SWI_GetTicks, 0x6d   @get start time
-
         .equ LEFT_BLACK_BUTTON, 0x01    @bit patterns for black buttons
         .equ RIGHT_BLACK_BUTTON, 0x02
-
         .equ BLUE_KEY_00, 0x01 @button(0)
         .equ BLUE_KEY_01, 0x02 @button(1)
         .equ BLUE_KEY_02, 0x04 @button(2)
@@ -18,27 +17,35 @@
         .equ BLUE_KEY_05, 0x20 @button(5)
         .equ BLUE_KEY_06, 0x40 @button(6)
         .equ BLUE_KEY_07, 0x80 @button(7)
-
         .text
         .global _start
 
 _start:
         @ initial
-INITIAL:swi   SWI_CLEAR_DISPLAY
+INITIAL:
+        swi   SWI_CLEAR_DISPLAY
         mov   r7, #0       @Length - 1
         mov   r6, #4       @direction   Up--1; Down--2; Left--3; Right--4
         mov   r5, #0       @count cycles
+        mov   r4, #0       @fruit count
 
         ldr   r0, =AddressX     @x
         ldr   r1, [r0]
+        mov   r3, #0
+        strb  r3, [r1]        @ start X
 
         ldr   r0, =AddressY     @y
         ldr   r2, [r0]
-
-        mov   r3, #0
-        strb  r3, [r1]        @ start X
         mov   r3, #8
         strb  r3, [r2]        @ start Y
+
+        ldr   r0, =FruitX     @Fruit-x
+        ldrb  r0, [r0]
+        ldr   r1, =FruitY     @Fruit-x
+        ldrb  r1, [r1]
+        mov   r2, #'#
+        swi   SWI_DRAW_CHAR
+
 
 
 L1:     swi   SWI_CheckBlue
@@ -52,13 +59,21 @@ L1:     swi   SWI_CheckBlue
         beq   SIX
         bal   M1
 
-ONE:    mov   r6, #1
+ONE:    cmp   r6, #2        @ When move up, can't move down
+        beq   M1
+        mov   r6, #1
         bal   M1
-FIVE:   mov   r6, #2
+FIVE:   cmp   r6, #1
+        beq   M1
+        mov   r6, #2
         bal   M1
-FOUR:   mov   r6, #3
+FOUR:   cmp   r6, #4
+        beq   M1
+        mov   r6, #3
         bal   M1
-SIX:    mov   r6, #4
+SIX:    cmp   r6, #3
+        beq   M1
+        mov   r6, #4
         @bal   M1
 
 
@@ -71,7 +86,11 @@ M1:     cmp   r5, #5
         cmp   r6, #0
         beq   OVER
 
-        mov     R5, #0
+        bl    CKFruit
+        cmp   r4, #6
+        beq   WIN
+
+        mov   R5, #0
         add   r7, r7, #1
         @Write
         ldr   r3, =AddressX     @x
@@ -80,7 +99,13 @@ M1:     cmp   r5, #5
         ldr   r3, [r3]
         strb  r0, [r2,r7]        @ start X
         strb  r1, [r3,r7]        @ start Y
-        ldr   r3, =500
+
+        ldr   r3, =SC     @Score+5
+        ldr   r1, [r3]
+        add   r1, r1, #5
+        strb  r1, [r3]        @ start X
+
+        ldr   r3, =300
         bl    Wait
 
         bal   L1
@@ -103,6 +128,10 @@ CRUISE:
         bl    CGXOY
         cmp   r6, #0
         beq   OVER
+        bl    CKFruit
+        cmp   r4, #6
+        beq   WIN
+
 
         @Write
         ldr   r3, =AddressX     @x
@@ -114,26 +143,61 @@ CRUISE:
 
         add   r5, r5, #1        @count++
 
-        ldr   r3, =500
+        ldr   r3, =300
         bl    Wait
 
 
         bal   L1
 
 
-OVER:   swi   SWI_CLEAR_DISPLAY
-        mov   r0, #17
-        mov   r1, #3
-        ldr   r2, =Die
+WIN:    swi   SWI_CLEAR_DISPLAY
+        mov   r0, #12
+        mov   r1, #5
+        ldr   r2, =Win
         swi   SWI_DRAW_STRING
 
-        mov   r0, #17
-        mov   r1, #5
+        mov   r0, #5
+        mov   r1, #7
+        ldr   r2, =Eat
+        swi   SWI_DRAW_STRING
+
+        mov   r0, #12
+        mov   r1, #9
         ldr   r2, =Score
         swi   SWI_DRAW_STRING
 
+        ldr   r3, =SC     @x
+        ldr   r2, [r3]
+        mov   r0, #19
+        mov   r1, #9
+        swi   SWI_DRAW_INT
+
         mov   r0, #1
+        mov   r1, #11
+        ldr   r2, =Restart
+        swi   SWI_DRAW_STRING
+        bal   CKBLK
+
+
+OVER:   swi   SWI_CLEAR_DISPLAY
+        mov   r0, #12
+        mov   r1, #5
+        ldr   r2, =Die
+        swi   SWI_DRAW_STRING
+
+        mov   r0, #12
         mov   r1, #7
+        ldr   r2, =Score
+        swi   SWI_DRAW_STRING
+
+        ldr   r3, =SC     @x
+        ldr   r2, [r3]
+        mov   r0, #19
+        mov   r1, #7
+        swi   SWI_DRAW_INT
+
+        mov   r0, #1
+        mov   r1, #9
         ldr   r2, =Restart
         swi   SWI_DRAW_STRING
 
@@ -144,6 +208,46 @@ CKBLK:  swi   SWI_CheckBlcak
 
         swi   SWI_Exit
 
+
+@ ====== Check Fruit (r0:x, r1:y)
+@ Determining whether fruit is obtained
+CKFruit:
+        stmfd sp!,{r0-r3,lr}
+
+        @Read
+        ldr   r2, =FruitX     @Fruit-x
+        ldrb  r2, [r2, r4]
+        cmp   r0, r2
+        bne   XCKFruit
+        ldr   r3, =FruitY     @Fruit-x
+        ldrb  r3, [r3, r4]
+        cmp   r1, r3
+        bne   XCKFruit
+
+        @Clear old fruit
+        mov   r0, r2
+        mov   r1, r3
+        mov   r2, #'*       @*
+        swi   SWI_DRAW_CHAR
+
+        add   r4, r4, #1
+
+        ldr   r3, =SC     @Score+10
+        ldr   r1, [r3]
+        add   r1, r1, #10
+        strb  r1, [r3]
+
+        @Display new fruit
+        ldr   r0, =FruitX     @Fruit-x
+        ldrb  r0, [r0, r4]
+        ldr   r1, =FruitY     @Fruit-x
+        ldrb  r1, [r1, r4]
+        mov   r2, #'#
+        swi   SWI_DRAW_CHAR
+
+XCKFruit:
+        ldmfd sp!,{r0-r3,pc}
+@ ===========================================================
 
 @ ====== Flush Memory (len:r7)
 @ Change coordinates of snake's body
@@ -195,19 +299,23 @@ CGXOY:
         cmp   r0, #39
         beq   GG
         add   r0, r0, #1
+        bl    Collide
         bal   XCGXOY
 
 UP:     cmp   r1, #0
         beq   GG
         sub   r1, r1, #1
+        bl    Collide
         bal   XCGXOY    @draw new point
 DOWN:   cmp   r1, #14
         beq   GG
         add   r1, r1, #1
+        bl    Collide
         bal   XCGXOY
 LEFT:   cmp   r0, #0
         beq   GG
         sub   r0, r0, #1
+        bl    Collide
 
 XCGXOY:
         mov   r2, #'*
@@ -218,7 +326,42 @@ GG:     mov   r6, #0
         ldmfd sp!,{pc}
 
 @ ===========================================================
+@ ====== Search (x:r0, y:r1, len:r7)
+@ Check whether collide
+Collide:
+        stmfd sp!,{r0-r5,r7,lr}
 
+        mov   r3, r0        @r3 <-- x
+        mov   r4, r1        @r4 <-- y
+
+        mov   r5, #0
+LP2:    cmp   r5, r7
+        bge   XCollide            @r5 >= r7
+
+        @Read
+        ldr   r2, =AddressX     @x
+        ldr   r2, [r2]
+        ldrb   r0, [r2, r5]     @get X
+
+        cmp   r0, r3
+        bne   LP3
+
+        ldr   r2, =AddressY     @y
+        ldr   r2, [r2]
+        ldrb   r1, [r2, r5]    @get Y
+
+        cmp   r1, r4
+        beq   CED
+
+LP3:
+        add   r5, r5, #1
+        bal   LP2
+XCollide:
+        ldmfd sp!,{r0-r5,r7,pc}
+
+CED:    mov   r6, #0
+        ldmfd sp!,{r0-r5,r7,pc}
+@ ===========================================================
 @ ====== Wait (Dealy:r3) wait for r3 milliseconds
 @ Delays for the amount of time stored in r3 for a 15-bit timer
 Wait:
@@ -244,10 +387,24 @@ XWait:
 @ ===========================================================
 
         .data
+Win:    .asciz  "!!!WIN!!!"
+Eat:    .asciz  "You have eaten all Fruits!"
 Die:    .asciz  "You died!"
 Score:  .asciz  "Score:"
 Restart:.asciz  "Press left black button to restart."
-Length: .word 0
-AddressX:   .word 0x2000
+SC:     .word 0
+FruitX:
+        .byte 29
+        .byte 10
+        .byte 31
+        .byte 32
+        .byte 24
+FruitY:
+        .byte 6
+        .byte 7
+        .byte 5
+        .byte 14
+        .byte 10
+AddressX:   .word 0x5000
 AddressY:   .word 0x6000
         .end
